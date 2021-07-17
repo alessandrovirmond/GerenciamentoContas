c7e39811794351908aa837f7372c8b4ef83d2179
Alessandro <avirmond2000@gmail.com>
2021-07-17 18:06:45 -0300

    implementado verificacao do limite para o saque

--- conta.py
+++ conta.py
@@ -1,5 +1,4 @@
 
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
@@ -16,10 +15,36 @@ class Conta:
         self.__saldo += valor
 
     def saca(self, valor):
-        self.__saldo -= valor
+        if(valor <= self.__saldo + self.__limite):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou do limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
         print("transferido {} para {}".format(valor, destino))
+
+    def get_saldo(self):
+        return  self.__saldo
+
+    def get_titular(self):
+        return self._titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+            return {'BB':'001', 'Caixa':'104', 'Bradesco':'237'}
+
 
